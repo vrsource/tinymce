43050fdeb799b3cf69e8c838a48a540208cbeb27
Nathan <my8bird@gmail.com>
2012-11-03 20:12:26 -0500

    Add prebuilt versions to "link" against.

---
+++ dist/tinymce/jscripts/tiny_mce/classes/util/VK.js
@@ -0,0 +1,26 @@
+/**
+ * This file exposes a set of the common KeyCodes for use.  Please grow it as needed.
+ */
+
+(function(tinymce){
+	tinymce.VK = {
+		BACKSPACE: 8,
+		DELETE: 46,
+		DOWN: 40,
+		ENTER: 13,
+		LEFT: 37,
+		RIGHT: 39,
+		SPACEBAR: 32,
+		TAB: 9,
+		UP: 38,
+
+		modifierPressed: function (e) {
+			return e.shiftKey || e.ctrlKey || e.altKey;
+		},
+
+		metaKeyPressed: function(e) {
+			// Check if ctrl or meta key is pressed also check if alt is false for Polish users
+			return tinymce.isMac ? e.metaKey : e.ctrlKey && !e.altKey;
+		}
+	};
+})(tinymce);
